8d90fc046d7cc53abca576441ec1f25e3ebab5f7
Sourabh Verma <sourabh.coder@gmail.com>
2014-10-15 19:17:21 +0530

    Add Layout

---
+++ app/src/main/java/contagious/apps/memorizer/MainActivity.java
@@ -0,0 +1,15 @@
+package contagious.apps.memorizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
